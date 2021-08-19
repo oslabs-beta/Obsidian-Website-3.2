a6e973d3808825d951634ec5b128c575b6b8496b
cjamesb <cjberry1234@sbcglobal.net>
2021-08-19 02:19:59 -0700

    push to aws?

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <img alt="Travis (.org)" src="https://img.shields.io/travis/lascaux-obsidian/obsidian-website">
 
 deno run --allow-net --allow-read --unstable server.tsx -c tsconfig.json
+
--- client/Components/Docs.tsx
+++ client/Components/Docs.tsx
@@ -1,25 +1,27 @@
-import { React } from '../../deps.ts';
+/** @format */
 
-import Overview from './DocPages/Overview.tsx';
-import Polling from './DocPages/ObsidianWrapper/Polling.tsx';
+import { React } from "../../deps.ts";
 
-import BasicsErrors from './DocPages/Basics/Errors.tsx';
-import GettingStarted from './DocPages/Basics/GettingStarted.tsx';
+import Overview from "./DocPages/Overview.tsx";
+import Polling from "./DocPages/ObsidianWrapper/Polling.tsx";
 
-import Mutations from './DocPages/Basics/Mutations.tsx';
-import Queries from './DocPages/Basics/Queries.tsx';
-import ServerSideRendering from './DocPages/Basics/ServerSideRendering.tsx';
+import BasicsErrors from "./DocPages/Basics/Errors.tsx";
+import GettingStarted from "./DocPages/Basics/GettingStarted.tsx";
 
-import Client from './DocPages/Caching/Client.tsx';
-import CachingErrors from './DocPages/Caching/Errors.tsx';
+import Mutations from "./DocPages/Basics/Mutations.tsx";
+import Queries from "./DocPages/Basics/Queries.tsx";
+import ServerSideRendering from "./DocPages/Basics/ServerSideRendering.tsx";
 
-import Server from './DocPages/Caching/Server.tsx';
+import Client from "./DocPages/Caching/Client.tsx";
+import CachingErrors from "./DocPages/Caching/Errors.tsx";
 
-import Strategies from './DocPages/Caching/Strategies.tsx';
-import Philosophy from './DocPages/Philosophy.tsx';
-import QuickStart from './DocPages/QuickStart.tsx';
+import Server from "./DocPages/Caching/Server.tsx";
 
-import SideBar from './SideBar.tsx';
+import Strategies from "./DocPages/Caching/Strategies.tsx";
+import Philosophy from "./DocPages/Philosophy.tsx";
+import QuickStart from "./DocPages/QuickStart.tsx";
+
+import SideBar from "./SideBar.tsx";
 
 declare global {
   namespace JSX {
@@ -30,35 +32,35 @@ declare global {
 }
 
 const Docs = (props: any) => {
-  const [docsPage, setDocsPage] = (React as any).useState('QuickStart');
+  const [docsPage, setDocsPage] = (React as any).useState("QuickStart");
 
   let curDocsPage;
-  if (docsPage === 'QuickStart') curDocsPage = <QuickStart />;
-  if (docsPage === 'Overview') curDocsPage = <Overview />;
-  if (docsPage === 'Philosophy') curDocsPage = <Philosophy />;
+  if (docsPage === "QuickStart") curDocsPage = <QuickStart />;
+  if (docsPage === "Overview") curDocsPage = <Overview />;
+  if (docsPage === "Philosophy") curDocsPage = <Philosophy />;
 
-  if (docsPage === 'Polling') curDocsPage = <Polling />;
+  if (docsPage === "Polling") curDocsPage = <Polling />;
 
-  if (docsPage === 'BasicsErrors')
+  if (docsPage === "BasicsErrors")
     curDocsPage = <BasicsErrors setDocsPage={setDocsPage} />;
-  if (docsPage === 'GettingStarted')
+  if (docsPage === "GettingStarted")
     curDocsPage = <GettingStarted setDocsPage={setDocsPage} />;
-  if (docsPage === 'Mutations')
+  if (docsPage === "Mutations")
     curDocsPage = <Mutations setDocsPage={setDocsPage} />;
-  if (docsPage === 'Queries')
+  if (docsPage === "Queries")
     curDocsPage = <Queries setDocsPage={setDocsPage} />;
-  if (docsPage === 'ServerSideRendering') curDocsPage = <ServerSideRendering />;
+  if (docsPage === "ServerSideRendering") curDocsPage = <ServerSideRendering />;
 
-  if (docsPage === 'Client') curDocsPage = <Client setDocsPage={setDocsPage} />;
-  if (docsPage === 'CachingErrors') curDocsPage = <CachingErrors />;
-  if (docsPage === 'Server') curDocsPage = <Server />;
-  if (docsPage === 'Strategies')
+  if (docsPage === "Client") curDocsPage = <Client setDocsPage={setDocsPage} />;
+  if (docsPage === "CachingErrors") curDocsPage = <CachingErrors />;
+  if (docsPage === "Server") curDocsPage = <Server />;
+  if (docsPage === "Strategies")
     curDocsPage = <Strategies setDocsPage={setDocsPage} />;
 
   return (
     <>
-      <div className="mainContainer pinkATags">
-        <div style={{ height: '100%' }}>{curDocsPage}</div>
+      <div className='mainContainer pinkATags'>
+        <div style={{ height: "100%" }}>{curDocsPage}</div>
       </div>
       <SideBar
         page={props.page}
