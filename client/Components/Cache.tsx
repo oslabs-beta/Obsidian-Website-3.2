e11709b406d3add7313c6ea599130976cdfa705f
cjamesb <cjberry1234@sbcglobal.net>
2021-08-19 02:11:23 -0700

    pushing website to aws

--- client/Components/Cache.tsx
+++ client/Components/Cache.tsx
@@ -1,5 +1,7 @@
-import { React } from '../../deps.ts';
-import NestedCache from './NestedCache.tsx';
+/** @format */
+
+import { React } from "../../deps.ts";
+import NestedCache from "./NestedCache.tsx";
 
 declare global {
   namespace JSX {
@@ -12,21 +14,22 @@ declare global {
 
 const Cache = (props: any) => {
   const { cache } = props;
-
   const cachedPair: any = [];
 
   Object.entries(cache).forEach((pair, i, arr) => {
-      if(pair[0] === 'ROOT_MUTATION' || pair[0] === 'ROOT_QUERY') return;
-      cachedPair.push(
-        <p key={`pair${i}`}>
-          {pair[0]} : , <NestedCache key ={`NestedCache-${i}`} cache={pair[1]}/>
-        </p>
-      );
+    if (pair[0] === "ROOT_MUTATION" || pair[0] === "ROOT_QUERY") return;
+    cachedPair.push(
+      <p key={`pair${i}`}>
+        {pair[0]} : , <NestedCache key={`NestedCache-${i}`} cache={pair[1]} />
+      </p>
+    );
   });
 
   return (
     <>
-      {'{'}{cachedPair}{'}'}
+      {"{"}
+      {cachedPair}
+      {"}"}
     </>
   );
 };
